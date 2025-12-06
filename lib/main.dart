b5559cc9720e9519953019573e90a14992e8c756
Nouradumirieh <nouradumirieh@gmail.com>
2025-12-06 13:27:31 +0300

    Comment out main.dart code

--- lib/main.dart
+++ lib/main.dart
@@ -105,4 +105,23 @@ class _MyAppState extends State<MyApp> {
     
      
   }
+}/*
+import 'package:flutter/material.dart';
+import 'signup_page.dart';
+
+void main() {
+  runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignUpPage(),
+    );
+  }
+}
+*/
