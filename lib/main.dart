d05a41a1795f5f8b6a8163157c052129356d878b
DarkMI17 <88891610+DarkMI17@users.noreply.github.com>
2025-12-16 22:30:36 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'login.dart';
 import 'signup.dart';
+import 'homePage.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
